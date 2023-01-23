b44a9729eb024a06c995ae233999d275b4e43eb3
Clementine1703 <kamil.alimetov@mail.ru>
2023-01-23 23:06:38 +0300

    пофиксил баг с появлением "undefined" вместо текста

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,41 +17,6 @@ $(document).ready(function(){
 			});
       	} 
     });
-
-
-
-
-	// ——————————————————————————————————————————————————
-// TextScramble
-// —————————————————————————————————————————————————
-  
-  // ——————————————————————————————————————————————————
-  // Example
-  // ——————————————————————————————————————————————————
-  
-//   const phrases = [
-// 	'Neo,',
-// 	'sooner or later',
-// 	'you\'re going to realize',
-// 	'just as I did',
-// 	'that there\'s a difference',
-// 	'between knowing the path',
-// 	'and walking the path'
-//   ]
-  
-//   const el = document.querySelector('.dynamic-text')
-//   const fx = new TextScramble(el)
-  
-//   let counter = 0
-//   const next = () => {
-// 	fx.setText(phrases[counter]).then(() => {
-// 	  setTimeout(next, 800)
-// 	})
-// 	counter = (counter + 1) % phrases.length
-//   }
-  
-//   next()
-
 });
 
 class TextScramble {
